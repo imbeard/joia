a2a175a40dc2cc817929b5c43b8b44aa01e2e1f8
Rocco Punghellini <rocco.punghellini@gmail.com>
2025-08-28 20:13:14 +0200

    i18n fixes

---
+++ sveltekit-app/src/lib/components/svg/MenuIcon.svelte
@@ -0,0 +1,12 @@
+<svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g clip-path="url(#clip0_812_8201)">
+		<path d="M0 6.10276H16" stroke="#057953" stroke-width="2" stroke-miterlimit="10" />
+		<path d="M0 1.10276H16" stroke="#057953" stroke-width="2" stroke-miterlimit="10" />
+		<path d="M0 11.1028H16" stroke="#057953" stroke-width="2" stroke-miterlimit="10" />
+	</g>
+	<defs>
+		<clipPath id="clip0_812_8201">
+			<rect width="16" height="12" fill="white" transform="translate(0 0.102759)" />
+		</clipPath>
+	</defs>
+</svg>
