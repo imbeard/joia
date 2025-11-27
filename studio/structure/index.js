5b5c2280a39319dddde8c60a0d27b0af8746fa55
Rocco Punghellini <rocco.punghellini@gmail.com>
2025-11-27 10:27:47 +0100

    wrap pages structure sanity

--- studio/structure/index.js
+++ studio/structure/index.js
@@ -6,73 +6,73 @@ export const structure = (S, context) =>
     .id('root')
     .title('Content')
     .items([
-      S.documentTypeListItem('home')
-        .title('Home')
-        .child(S.document().schemaType('home').title('Home').documentId('home')),
+      S.listItem()
+        .title('Pages')
+        .icon(FolderIcon)
+        .child(
+          S.list()
+            .title('Pages')
+            .items([
+              S.documentTypeListItem('home')
+                .title('Home')
+                .child(S.document().schemaType('home').title('Home').documentId('home')),
+              S.documentTypeListItem('menuPage')
+                .title('Menu')
+                .child(S.document().schemaType('menuPage').title('Menu').documentId('menuPage')),
+              S.documentTypeListItem('wines')
+                .title('Wines')
+                .child(S.document().schemaType('wines').title('Wines').documentId('wines')),
+              S.documentTypeListItem('softDrinks')
+                .title('Soft Drinks')
+                .child(
+                  S.document()
+                    .schemaType('softDrinks')
+                    .title('Soft Drinks')
+                    .documentId('soft-drinks'),
+                ),
+              S.documentTypeListItem('herbalTeas')
+                .title('Herbal Teas')
+                .child(
+                  S.document()
+                    .schemaType('herbalTeas')
+                    .title('Herbal Teas')
+                    .documentId('herbal-teas'),
+                ),
+              S.documentTypeListItem('drinking')
+                .title('Drinking')
+                .child(
+                  S.document().schemaType('drinking').title('Drinking').documentId('drinking'),
+                ),
+              S.documentTypeListItem('lunch')
+                .title('Lunch')
+                .child(S.document().schemaType('lunch').title('Lunch').documentId('lunch')),
+              S.documentTypeListItem('about')
+                .title('About')
+                .child(S.document().schemaType('about').title('About').documentId('about')),
+
+              S.documentTypeListItem('events')
+                .title('Events')
+                .child(S.document().schemaType('events').title('Events').documentId('events')),
+
+              S.documentTypeListItem('academy')
+                .title('Academy')
+                .child(S.document().schemaType('academy').title('Academy').documentId('academy')),
+            ]),
+        ),
 
       S.divider(),
 
-      S.documentTypeListItem('menuPage')
-        .title('Menu Page')
-        .child(S.document().schemaType('menuPage').title('Menu Page').documentId('menuPage')),
-
-      S.documentTypeListItem('menu').title('Menus'),
-
-      S.divider(),
-
-      S.documentTypeListItem('wines')
-        .title('Wines Page')
-        .child(S.document().schemaType('wines').title('Wines').documentId('wines')),
+      S.documentTypeListItem('menu').title('Menu List'),
 
       S.documentTypeListItem('wine').title('Wine List'),
 
-      S.divider(),
-
-      S.documentTypeListItem('softDrinks')
-        .title('Soft Drinks Page')
-        .child(
-          S.document().schemaType('softDrinks').title('Soft Drinks').documentId('soft-drinks'),
-        ),
-
       S.documentTypeListItem('softDrink').title('Soft Drink List'),
-
-      S.divider(),
-
-      S.documentTypeListItem('herbalTeas')
-        .title('Herbal Teas Page')
-        .child(
-          S.document().schemaType('herbalTeas').title('Herbal Teas').documentId('herbal-teas'),
-        ),
 
       S.documentTypeListItem('herbalTea').title('Herbal Tea List'),
 
-      S.divider(),
-
-      S.documentTypeListItem('drinking')
-        .title('Drinking')
-        .child(S.document().schemaType('drinking').title('Drinking').documentId('drinking')),
-
-      S.documentTypeListItem('lunch')
-        .title('Lunch')
-        .child(S.document().schemaType('lunch').title('Lunch').documentId('lunch')),
-
-      S.divider(),
-
-      S.documentTypeListItem('about')
-        .title('About')
-        .child(S.document().schemaType('about').title('About').documentId('about')),
-
-      S.documentTypeListItem('events')
-        .title('Events')
-        .child(S.document().schemaType('events').title('Events').documentId('events')),
-
-      S.documentTypeListItem('academy')
-        .title('Academy')
-        .child(S.document().schemaType('academy').title('Academy').documentId('academy')),
-
       orderableDocumentListDeskItem({
         type: 'press',
-        title: 'Press',
+        title: 'Press List',
         icon: FolderIcon,
         S,
         context,
